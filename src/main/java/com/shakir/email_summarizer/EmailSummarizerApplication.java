77bc227950efb0da119662c70f2052515b15661f
Shakir <s.shakirz26@gmail.com>
2025-09-05 16:56:14 +0530

    START

---
+++ src/main/java/com/shakir/email_summarizer/EmailSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.shakir.email_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shakir/email_summarizer/EmailSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakir.email_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
